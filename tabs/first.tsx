6e404e378230bca46addbb088d3a0c113c6598de
1024小神 <15670339118@qq.com>
2024-03-09 15:20:31 +0800

    ad

--- tabs/first.tsx
+++ tabs/first.tsx
@@ -6,114 +6,114 @@ import { Collapse, Button } from 'antd'
 
 // 初始化仓库存储
 const storage = new Storage({
-    area: 'local',
-    copiedKeyList: ['shield-modulation'],
+  area: 'local',
+  copiedKeyList: ['shield-modulation']
 })
 
-chrome.storage.local.set({ 'shield-modulation': '11111111111' })
+// chrome.storage.local.set({ 'shield-modulation': '11111111111' })
 
 // 显示已经发过的文章缓存
 function DeltaFlyerPage() {
-    const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-    const [items, setItems]: any = useState([])
+  const [items, setItems]: any = useState([])
 
-    const genExtra = (item: any) => (
-        <div className="syncBox">
-            <div
-                className="syncItem"
-                onClick={async (event) => {
-                    // If you don't want click extra trigger collapse, you can prevent this:
-                    event.stopPropagation()
-                    console.log('点击了一篇文章:', item)
-                    await storage.setItem('one', item)
-                    creatJuejin('https://juejin.cn/editor/drafts/new')
-                }}
-            >
-                <ArrowRightOutlined />
-                <span>掘金</span>
-            </div>
-            <div className="syncItem">
-                <ArrowRightOutlined
-                    onClick={(event) => {
-                        // If you don't want click extra trigger collapse, you can prevent this:
-                        event.stopPropagation()
-                        console.log('点击了一篇文章:', item)
-                    }}
-                />
-                <span>博客园</span>
-            </div>
-        </div>
-    )
+  const genExtra = (item: any) => (
+    <div className='syncBox'>
+      <div
+        className='syncItem'
+        onClick={async (event) => {
+          // If you don't want click extra trigger collapse, you can prevent this:
+          event.stopPropagation()
+          console.log('点击了一篇文章:', item)
+          await storage.setItem('one', item)
+          creatJuejin('https://juejin.cn/editor/drafts/new')
+        }}
+      >
+        <ArrowRightOutlined />
+        <span>掘金</span>
+      </div>
+      <div className='syncItem'>
+        <ArrowRightOutlined
+          onClick={(event) => {
+            // If you don't want click extra trigger collapse, you can prevent this:
+            event.stopPropagation()
+            console.log('点击了一篇文章:', item)
+          }}
+        />
+        <span>博客园</span>
+      </div>
+    </div>
+  )
 
-    // 初始化页面数据
-    const initData = async () => {
-        const articles: [] = await storage.getItem('articles')
-        console.log('加载缓存的文章:', storage, articles)
-        if (articles && articles.length > 0) {
-            const artList = articles.map((item: any, index) => {
-                return {
-                    key: index,
-                    label: item.title,
-                    children: <p>{item.content}</p>,
-                    extra: genExtra(item),
-                }
-            })
-            setItems(artList)
+  // 初始化页面数据
+  const initData = async () => {
+    const articles: [] = await storage.getItem('articles')
+    console.log('加载缓存的文章:', storage, articles)
+    if (articles && articles.length > 0) {
+      const artList = articles.map((item: any, index) => {
+        return {
+          key: index,
+          label: item.title,
+          children: <p>{item.content}</p>,
+          extra: genExtra(item)
         }
+      })
+      setItems(artList)
     }
+  }
 
-    // 清空数据
-    const clearData = async () => {
-        await storage.removeItem('articles')
-        setItems([])
-    }
+  // 清空数据
+  const clearData = async () => {
+    await storage.removeItem('articles')
+    setItems([])
+  }
 
-    // 给掘金发消息
-    const sendMessage = () => {
-        console.log('给掘金发消息')
-        // chrome.tabs.sendMessage(
-        //   tabId: 12132,
-        //   message: "any",
-        //   callback: ()=>{},
-        // )
-    }
+  // 给掘金发消息
+  const sendMessage = () => {
+    console.log('给掘金发消息')
+    // chrome.tabs.sendMessage(
+    //   tabId: 12132,
+    //   message: "any",
+    //   callback: ()=>{},
+    // )
+  }
 
-    // 创建掘金tab
-    const creatJuejin = (url?: string) => {
-        chrome.windows.create(
-            {
-                url: url,
-                type: 'normal',
-                state: 'minimized',
-            },
-            (win) => {
-                console.log('windows创建成功', win)
-            }
-        )
-    }
+  // 创建掘金tab
+  const creatJuejin = (url?: string) => {
+    chrome.windows.create(
+      {
+        url: url,
+        type: 'normal',
+        state: 'minimized'
+      },
+      (win) => {
+        console.log('windows创建成功', win)
+      }
+    )
+  }
 
-    useEffect(() => {
-        initData()
-    }, [])
+  useEffect(() => {
+    initData()
+  }, [])
 
-    return (
-        <>
-            <div className="titleBox">
-                <h2 className="label">缓存的文章列表</h2>
-                <Button type="primary" className="btn" onClick={clearData}>
-                    清空数据
-                </Button>
-                {/* <Button type="primary" className="btn">
+  return (
+    <>
+      <div className='titleBox'>
+        <h2 className='label'>缓存的文章列表</h2>
+        <Button type='primary' className='btn' onClick={clearData}>
+          清空数据
+        </Button>
+        {/* <Button type="primary" className="btn">
                     打开掘金
                 </Button> */}
-                <Button type="primary" className="btn" onClick={clearData}>
-                    给掘金发消息
-                </Button>
-            </div>
-            <Collapse accordion items={items} />
-        </>
-    )
+        <Button type='primary' className='btn' onClick={clearData}>
+          给掘金发消息
+        </Button>
+      </div>
+      <Collapse accordion items={items} />
+    </>
+  )
 }
 
 export default DeltaFlyerPage
