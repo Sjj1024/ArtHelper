1201183d66506750113157ce2dc099fe13d71d70
1024小神 <15670339118@163.com>
2024-03-29 10:06:53 +0800

    add

--- contents/all.ts
+++ contents/all.ts
@@ -26,37 +26,8 @@ const listenDom = () => {
         listenCsdnTitle(curUrl)
         listenCsdnContent(curUrl)
         // clickNum(curUrl)
-        for (var mutation of mutationsList) {
-            // 如果type是childList说明是元素添加或者删除了
-            if (mutation.type == 'childList') {
-                //  remove csdn ad
-                if (curUrl.includes('mp.csdn.net/mp_blog/manage/article')) {
-                    // console.log('移除垃圾弹窗广告.', document.querySelector('.tip-box'))
-                    if (document.getElementById('nps-box')) {
-                        document.getElementById('nps-box').style.display =
-                            'none'
-                    }
-                    if (document.querySelector('.tip-box')) {
-                        const parent =
-                            document.querySelector('.tip-box').parentElement
-                        parent.removeChild(parent.lastElementChild)
-                    }
-                    if (document.querySelector('.traffic-show-box')) {
-                        ;(
-                            document.querySelector(
-                                '.traffic-show-box'
-                            ) as HTMLElement
-                        ).style.display = 'none'
-                    }
-                }
-
-                // document.getElementById('nps-box').style.display = 'none'
-            } else if (mutation.type == 'attributes') {
-                // console.log('样式发生了变化', mutation)
-                if (document.getElementById('nps-box')) {
-                    document.getElementById('nps-box').style.display = 'none'
-                }
-            }
+        if (curUrl.includes('mp.csdn.net/mp_blog')) {
+            csdnHandle()
         }
     }
     // 创建一个observer示例与回调函数相关联
@@ -69,6 +40,46 @@ const listenDom = () => {
 
 // 监听所有页面加载完成
 listenDom()
+
+// csdn handle
+const csdnHandle = () => {
+    // clear ad
+    csdnClearAd()
+    // csdn footer add juejin category
+    csdnFooter()
+}
+
+// csdn ad clear
+const csdnClearAd = () => {
+    // console.log('移除垃圾弹窗广告.', document.querySelector('.tip-box'))
+    if (document.getElementById('nps-box')) {
+        document.getElementById('nps-box').style.display = 'none'
+    }
+    if (document.querySelector('.tip-box')) {
+        const parent = document.querySelector('.tip-box').parentElement
+        parent.removeChild(parent.lastElementChild)
+    }
+    if (document.querySelector('.traffic-show-box')) {
+        ;(
+            document.querySelector('.traffic-show-box') as HTMLElement
+        ).style.display = 'none'
+    }
+}
+
+// csdn footer add juejin info
+const csdnFooter = () => {
+    // add category
+    const footer = document.querySelector('#moreDiv')
+    const catggory = document.querySelector('#juejinCategory')
+    if (footer && !catggory) {
+        // catrgory
+        const divCategory = document.createElement('div')
+        divCategory.id = 'juejinCategory'
+        const labelCategory = `<div data-v-00bc4474="" class="el-form-item mt8 is-no-asterisk"><label class="el-form-item__label" style="width: 90px;"><span data-v-00bc4474="" class="el-tooltip" aria-describedby="el-tooltip-4452" tabindex="0"> 掘金分类 </span></label><div class="el-form-item__content" style="margin-left: 90px;"><label data-v-00bc4474="" role="radio" aria-checked="true" tabindex="0" class="el-radio originalRadio is-checked"><span class="el-radio__input is-checked"><span class="el-radio__inner"></span><input type="radio" aria-hidden="true" tabindex="-1" autocomplete="off" class="el-radio__original" value="original"></span><span class="el-radio__label"><span data-v-00bc4474="" class="el-tooltip item" aria-describedby="el-tooltip-3256" tabindex="0">原创<i data-v-00bc4474="" class="tortImg"></i></span><!----></span></label><label data-v-00bc4474="" role="radio" tabindex="0" class="el-radio"><span class="el-radio__input"><span class="el-radio__inner"></span><input type="radio" aria-hidden="true" tabindex="-1" autocomplete="off" class="el-radio__original" value="repost"></span><span class="el-radio__label"><span data-v-00bc4474="" class="el-tooltip item" aria-describedby="el-tooltip-6972" tabindex="0">转载</span><!----></span></label><label data-v-00bc4474="" role="radio" tabindex="0" class="el-radio"><span class="el-radio__input"><span class="el-radio__inner"></span><input type="radio" aria-hidden="true" tabindex="-1" autocomplete="off" class="el-radio__original" value="translated"></span><span class="el-radio__label"><span data-v-00bc4474="" class="el-tooltip item" aria-describedby="el-tooltip-8430" tabindex="0">翻译</span><!----></span></label><!----></div></div>`
+        divCategory.innerHTML = labelCategory
+        footer.appendChild(divCategory)
+    }
+}
 
 // 测试点击事件有多少个
 var isChangeEventBound = true // 初始化标志变量
